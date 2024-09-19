76d179d92b667d906814f4dea176a0211fd66097
brandon.lockridge <brandon.lockridge@awarehq.com>
2024-09-19 09:42:00 -0400

    data processing for match shots

--- src/data_processing/sca_player_shots/sca_player_processing.py
+++ src/data_processing/sca_player_shots/sca_player_processing.py
@@ -26,8 +26,8 @@ class SCAPlayerShotsUtil:
                     get_sca_player_1_id_query = "select player_id from players where player_name = (%s)"
                     get_sca_player_1_id_parameters = (sca_player_1_name,)
                     sca_player_1_id = postgres_connector.execute_parameterized_select_query(get_sca_player_1_id_query, get_sca_player_1_id_parameters)[0]
-                    update_sca_player_1_id_query = "UPDATE match_shots_stats SET sca1_player_id = (%s) WHERE sca_1_player = (%s);"
-                    update_sca_player_1_id_parameters = (sca_player_1_id, sca_player_1_name)
+                    update_sca_player_1_id_query = "UPDATE match_shots_stats SET sca1_player_id = (%s) WHERE sca_1_player = (%s) and shot_id = (%s);"
+                    update_sca_player_1_id_parameters = (sca_player_1_id, sca_player_1_name, shot_id)
                     print("Updating SCA1: " + str(update_sca_player_1_id_parameters))
                     postgres_connector.execute_parameterized_insert_query(update_sca_player_1_id_query, update_sca_player_1_id_parameters)
             except Exception as e:
@@ -35,21 +35,49 @@ class SCAPlayerShotsUtil:
                 error_shots.append(shot_id)
             try:
                 if sca_player_2_name != '':
-                    get_sca_player_2_id_query = "select player_id from players where player_name = (%s)"
+                    get_sca_player_2_id_query = "select player_id from players where player_name = (%s) "
                     get_sca_player_2_id_parameters = (sca_player_2_name,)
                     sca_player_2_id = postgres_connector.execute_parameterized_select_query(get_sca_player_2_id_query, get_sca_player_2_id_parameters)[0]
-                    update_sca_player_2_id_query = "UPDATE match_shots_stats SET sca2_player_id = (%s) WHERE sca_2_player = (%s);"
-                    update_sca_player_2_id_parameters = (sca_player_2_id, sca_player_2_name)
+                    update_sca_player_2_id_query = "UPDATE match_shots_stats SET sca2_player_id = (%s) WHERE sca_2_player = (%s) and shot_id = (%s);"
+                    update_sca_player_2_id_parameters = (sca_player_2_id, sca_player_2_name, shot_id)
                     postgres_connector.execute_parameterized_insert_query(update_sca_player_2_id_query, update_sca_player_2_id_parameters)
                     print("Updating SCA2: " + str(update_sca_player_2_id_parameters))
             except Exception as e:
                 print("Error updating SCA2 id for player: " + sca_player_2_name)
                 error_shots.append(shot_id)
-        print("error shots: \n" + error_shots)
+        print("error shots: \n" + str(len(error_shots)))
+
+    @staticmethod
+    def remove_blank_sca_player_ids(postgres_connector=None):
+        if(postgres_connector is None):
+            postgres_connector = PostgresConnector()
+            postgres_connector.open_connection_cursor("premier_league_stats")
+
+        select_query = "select sca_1_player, sca_2_player, shot_id  from match_shots_stats"
+        select_parameters = ()
+
+        all_match_shots = postgres_connector.execute_parameterized_select_query(select_query, select_parameters)
+        for shot in all_match_shots:
+            sca_player_1_name = shot[0]
+            sca_player_2_name = shot[1]
+            shot_id = shot[2]
+
+            if sca_player_1_name == '':
+                update_sca_player_1_id_query = "UPDATE match_shots_stats SET sca1_player_id = (%s) WHERE sca_1_player = (%s) and shot_id = (%s);"
+                update_sca_player_1_id_parameters = ('', sca_player_1_name, shot_id)
+                postgres_connector.execute_parameterized_insert_query(update_sca_player_1_id_query, update_sca_player_1_id_parameters)
+                print("Updating blank sca1 of shot: " + str(shot_id))
+            if sca_player_2_name == '':
+                update_sca_player_2_id_query = "UPDATE match_shots_stats SET sca2_player_id = (%s) WHERE sca_2_player = (%s) and shot_id = (%s);"
+                update_sca_player_2_id_parameters = ('', sca_player_2_name, shot_id)
+                postgres_connector.execute_parameterized_insert_query(update_sca_player_2_id_query, update_sca_player_2_id_parameters)
+                print("Updating blank sca2 of shot: " + str(shot_id))
 
 
-os.environ['DB_PASS'] = "MySampleThing!#"
-SCAPlayerShotsUtil.update_sca_player_ids()
+
+
+
+
 
 
 
